b8e675712f0fdf42ab96f30f1176f745fb6488f6
Dung-L3 <donnylee2003@gmail.com>
2025-07-18 20:11:52 +0700

    Update TableOrderRequest.java

--- management/src/main/java/com/system/restaurant/management/dto/TableOrderRequest.java
+++ management/src/main/java/com/system/restaurant/management/dto/TableOrderRequest.java
@@ -1,13 +1,14 @@
 package com.system.restaurant.management.dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TableOrderRequest {
-    @NotNull(message = "Table ID is required")
-    private Integer tableId;
-
-    @NotNull(message = "Order item is required")
+    @Valid
+    @NotNull(message = "Item details are required")
     private OrderItemRequest item;
+
+    private Integer tableId;
 }
