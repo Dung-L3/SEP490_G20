58d5143e1910f45c1bb0cdbed0ee13b117b9c7f3
Dung-L3 <donnylee2003@gmail.com>
2025-07-22 22:15:06 +0700

    update dish controller

--- management/src/main/java/com/system/restaurant/management/controller/StaffController.java
+++ management/src/main/java/com/system/restaurant/management/controller/StaffController.java
@@ -13,7 +13,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/staffs")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class StaffController {
 
     private final StaffService staffService;
