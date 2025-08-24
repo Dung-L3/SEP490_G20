37aeb64224155ef0f437a2d26b9385073fa0aaa0
Dau Ba Anh Dung <anhdungkudo@gmail.com>
2025-08-24 14:43:31 +0700

    Update App.tsx

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -34,7 +34,7 @@ import ResetPassword  from './pages/ResetPassword';
 import ComboManager from './pages/manager/ComboManager';
 import PromotionsManager from './pages/manager/PromotionsManager';
 //import TakeawayOrderPublic from './pages/TakeawayOrderPublic';
-// import TakeawayOrderPublic from './pages/TakeawayOrderPublic';
+
 
 function App() {
   return (
@@ -69,8 +69,7 @@ function App() {
           <Route path="/waiter/tables" element={<WaiterTableView />} /> {/* Route cho waiter xem bàn */}
           <Route path="/chef" element={<Chef />} /> {/* Route cho trang Chef (bếp trưởng) */}
            {/*<Route path="/takeaway-order" element={<TakeawayOrderPublic />} /> Route cho trang đơn hàng mang đi công khai*/}
-          {/* <Route path="/takeaway-order" element={<TakeawayOrderPublic />} /> Route cho trang đơn hàng mang đi công khai */}
-
+         
           {/* QR Menu Routes */}
           <Route path="/menu/:tableId" element={<QRMenu />} /> {/* Route cho trang QR menu khách hàng */}
           <Route path="/qr-manager" element={<QRCodeManager />} /> {/* Route cho trang quản lý QR codes */}
